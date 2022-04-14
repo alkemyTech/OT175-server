4d620e8f8eabd30ec58c6f78a5255e7703808c49
Rubens94 <ruben9_1994@hotmail.com>
2022-04-14 16:24:41 -0500

    fix name for class Member in models

--- models/member.js
+++ models/member.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class member extends Model {
+  class Member extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
