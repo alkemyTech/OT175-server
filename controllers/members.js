adccdb000a9c6aa10d94032074b545c927bb9766
Rubens94 <ruben9_1994@hotmail.com>
2022-04-14 17:18:05 -0500

    GET member by ID

--- controllers/members.js
+++ controllers/members.js
@@ -12,7 +12,7 @@ const getMembers = async(req, res) => {
 
         // Verify if exists members
         if ( !members ) {
-            return res.status(400).json({msg:'There is no registered member'});
+            return res.status(400).json({msg:'There is no registered members'});
         }
 
         res.status(200).json({members});
@@ -22,6 +22,30 @@ const getMembers = async(req, res) => {
     }
 }
 
+const getMember = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const member = await Member.findOne({
+            where: {
+                id,
+                'deleted': 0
+            },
+            attributes: ['id','name', 'facebookUrl', 'instagramUrl', 'linkedinUrl', 'image', 'description']
+        });
+
+        // Verify if exists members
+        if ( !member ) {
+            return res.status(400).json({msg:'There is no registered member'});
+        }
+
+        res.status(200).json({member});
+    } catch (err) {
+        console.log(err);
+        res.status(501).json({msg:'There was a problem getting the members, check with the administrator'})
+    }
+}
+
 module.exports = {
-    getMembers
+    getMembers,
+    getMember
 }
--- routes/members.js
+++ routes/members.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getMembers
+    getMembers,
+    getMember
 } = require('../controllers/members');
 
 /* GET members listing. */
-router.get('/', getMembers)
+router.get('/', getMembers);
+
+/* GET member by ID */
+router.get('/:id', getMember);
 
 module.exports = router;
