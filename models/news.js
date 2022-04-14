7bfb4e45d6137727eef257d929393c9c20559d65
alejo <alejorazeto@gmail.com>
2022-04-14 14:58:01 -0300

    news model created

---
+++ models/news.js
@@ -0,0 +1,44 @@
+const {Sequelize, Model, DataTypes} = require('sequelize');
+
+class News extends Model{
+    static associate(db){
+        this.belongsTo(db.Categories,{as:'category'})
+        //aclarar con estefany si es one-to-many o many-to-many 
+    }
+};
+
+const NewsSchema = {
+    id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name:{
+        allowNull:false,
+        type: DataTypes.TEXT
+    },
+    content:{
+        allowNull:false,
+        type: DataTypes.STRING
+    },
+    image:{
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    categoryId:{
+        allowNull:false,
+        type: DataTypes.INTEGER
+    }
+}
+
+News.init(
+    NewsSchema
+    ,{
+    sequelize,
+    modelName: 'News',
+    freezeTableName: true
+});
+
+module.exports= (NewsSchema, News);
+
