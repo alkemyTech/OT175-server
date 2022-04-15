cabf35c0960da1216f07d58ec57e7c8b935dca6a
Ariel Ruiz <soyarielruiz@gmail.com>
2022-04-14 22:54:09 -0300

    Revert "Ot175 22 members models migrations and controllers (#2)"
    This reverts commit ebdded862876c64193933c4df93c9b9589f3b48c.

--- controllers/members.js
+++ controllers/members.js
@@ -11,9 +11,8 @@ const getMembers = async(req, res) => {
         });
 
         // Verify if exists members
-        if ( members.length === 0 ) {
+        if ( !members ) {
             return res.status(400).json({msg:'There is no registered members'});
-            
         }
 
         res.status(200).json({members});
@@ -98,98 +97,9 @@ const deleteMember = async(req, res) => {
     }
 }
 
-const getMembersDeleted = async(req, res) => {
-    try {
-        const members = await Member.findAll({
-            where: {
-                'deleted': 1
-            },
-            attributes: ['id','name', 'facebookUrl', 'instagramUrl', 'linkedinUrl', 'image', 'description']
-        });
-
-        // Verify if exists members
-        if ( members.length === 0 ) {
-            return res.status(400).json({msg:'no members removed'});
-        }
-
-        res.status(200).json({members});
-    } catch (err) {
-        console.log(err);
-        res.status(501).json({msg:'There was a problem getting the members, check with the administrator'})
-    }
-}
-
-const putMembersDeleted = async(req, res) => {
-
-    try {
-        const { id } = req.params;
-
-        const member = await Member.findOne({ where: { id, 'deleted': 1 } });
-
-        // Verify if exists members
-        if ( !member ) {
-            return res.status(400).json({msg:'there is no deleted member with that ID'});
-        }
-
-        // Update data
-        member.update({
-            'deleted': 0
-        });
-
-        res.status(200).json({msg: 'Member restored'});
-    } catch (err) {
-        console.log(err);
-        res.status(501).json({msg:'There was a problem getting the members, check with the administrator'})
-    }
-}
-
-const deleteMembersDeleted = async(req,res) => {
-    try {
-        const { id } = req.params;
-
-        const member = await Member.findOne({ where: { id, 'deleted': 1 } });
-
-        // Verify if exists members
-        if ( !member ) {
-            return res.status(400).json({msg:'there is no deleted member with that ID'});
-        }
-
-        // Delete member
-        member.destroy();
-
-        res.status(200).json({msg: 'Member deleted'});
-    } catch (err) {
-        console.log(err);
-        res.status(501).json({msg:'There was a problem getting the members, check with the administrator'})
-    }
-}
-
-const postMember = async(req, res) => {
-    try {
-        const { name, facebookUrl, instagramUrl, linkedinUrl, image, description } = req.body;
-        await Member.create({
-            name,
-            facebookUrl,
-            instagramUrl,
-            linkedinUrl,
-            image,
-            description 
-        });
-
-        res.status(200).json({msg: 'Member created'});
-    } catch (err) {
-        console.log(err);
-        res.status(501).json({msg:'There was a problem getting the members, check with the administrator'})
-    }
-}
-
 module.exports = {
     getMembers,
     getMember,
     updateMember,
-    deleteMember,
-    getMembersDeleted,
-    putMembersDeleted,
-    deleteMembersDeleted,
-    postMember
+    deleteMember
 }
