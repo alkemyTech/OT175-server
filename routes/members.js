ebdded862876c64193933c4df93c9b9589f3b48c
Rubén Marroquín <52043211+Rubens94@users.noreply.github.com>
2022-04-14 20:01:23 -0500

    Ot175 22 members models migrations and controllers (#2)
    * middlewares folder added, validations with express-validator and routes for admin in members
    
    * routes and controller fixed

--- routes/members.js
+++ routes/members.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 const {
     getMembers,
     getMember,
     updateMember,
-    deleteMember
+    deleteMember,
+    getMembersDeleted,
+    putMembersDeleted,
+    deleteMembersDeleted,
+    postMember
 } = require('../controllers/members');
+
+const { fieldsValidate } = require('../middlewares/fieldsValidate');
 
 /* GET members listing. */
 router.get('/', getMembers);
+
+/* POST members. */
+router.post('/', [
+    check('name', 'name can´t be empty').not().isEmpty().trim().escape(),
+    check('name', 'name must be string').isString().trim().escape(),
+    check('facebookUrl').trim().escape(),
+    check('instagramUrl').trim().escape(),
+    check('linkedinUrl').trim().escape(),
+    check('image', 'image can´t be empty').not().isEmpty().trim().escape(),
+    check('image', 'image must be string').isString().trim().escape(),
+    check('description').trim().escape(),
+    fieldsValidate
+],postMember);
+
+/* GET members deleted. Only Admin can see */
+router.get('/admin/', getMembersDeleted);
+
+/* PUT members deleted. Only Admin can see */
+router.put('/admin/:id', putMembersDeleted);
+
+/* DELETE members deleted. Only Admin can see */
+router.delete('/admin/:id', deleteMembersDeleted);
 
 /* GET member by ID */
 router.get('/:id', getMember);
 
 /* PUT member by ID */
-router.put('/:id', updateMember);
+router.put('/:id', [
+    check('name', 'name can´t be empty').not().isEmpty().trim().escape(),
+    check('name', 'name must be string').isString().trim().escape(),
+    check('facebookUrl').trim().escape(),
+    check('instagramUrl').trim().escape(),
+    check('linkedinUrl').trim().escape(),
+    check('image', 'image can´t be empty').not().isEmpty().trim().escape(),
+    check('image', 'image must be string').isString().trim().escape(),
+    check('description').trim().escape(),
+    fieldsValidate
+],updateMember);
 
 /* DELETEE member by ID */
 router.delete('/:id', deleteMember);
