8424660ab8e8ac16265c525146b7b09a4d3d7bc0
alejo <alejorazeto@gmail.com>
2022-04-17 23:36:01 -0300

    delete endpoint and helper middleware

---
+++ middlewares/selfAuth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function selfAuth(req, res, next){
+    const {id} =req.params 
+
+    const token = req.header;
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    if(payload.id==id){
+        next()
+    }else{
+        res.status(501).send('conflict ocurred')
+    };
+};
+module.exports = selfAuth
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const selfAuth = require('../middlewares/selfAuth')
+const models = require('../models');
+const { User } = models
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.delete('/:id',
+  selfAuth,
+  async(req, res, next)=>{
+    const { id } = req.params
+    User.destroy({
+      where: {id:id}
+    })
+    .then(()=>res.status(200).send('deletion complete'))
+    .catch((err)=>{
+      console.error(err);
+      res.status(404).send('could not delete')
+    });
+  }
+)
 
 module.exports = router;
