f4ed00c2f936ab8fe8c9c7f856584034ecb6485c
AlRazeto <95495039+AlRazeto@users.noreply.github.com>
2022-04-17 21:23:55 -0300

    better explaining of the filter

--- middlewares/userDataFilter.js
+++ middlewares/userDataFilter.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function filterDataForUser(rules){
-//
+//rules bebería ser un objeto con roles y sus respectivos campos restingidos en un array.
     return async(req, res, next)=>{
         const {data}  = res.locals
         //debeíamos pasar los datos por médio de res.locals para que sean accesibles
@@ -10,8 +10,7 @@ function filterDataForUser(rules){
         //se supone que para llegar hasta este middleware el token fue previamente autenticado
         const role = token.payload.roleId 
         const restricted = rules[role]
-
-
+        //se usa el array de restricciones acorde con la key(de rules) que coincida
         for(blocked of restricted){
             delete data[blocked]
         }
