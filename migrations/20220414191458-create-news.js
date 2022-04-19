1521842735aac721e4eb1157b5a7db8e2b9591de
alejo <alejorazeto@gmail.com>
2022-04-19 13:13:58 -0300

    name conflict fix

--- migrations/20220414191458-create-news.js
+++ migrations/20220414191458-create-news.js
@@ -20,7 +20,7 @@ module.exports = {
         allowNull:false,
         type: Sequelize.STRING
       },
-      categoryId: {
+      category_id: {
         allowNull:false,
         type: Sequelize.INTEGER
       },
--- models/news.js
+++ models/news.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.STRING
     },
-    categoryId:{
+    category_id:{
         allowNull:false,
         type: DataTypes.INTEGER
     }
