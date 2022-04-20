e6c671ea16a9194386000222de0825e8e88a0137
alejo <alejorazeto@gmail.com>
2022-04-20 00:48:23 -0300

    tested with headers

--- middlewares/selfAuth.js
+++ middlewares/selfAuth.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 async function selfAuth(req, res, next){
-    const {id} =req.params 
+    const {id} = req.params 
 
-    const token = req.header;
+    const token = req.headers.authorization.slice(7);
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    if(payload.id===id){
+    if(payload.userId===parseInt(id)){
+        
         next()
     }else{
         res.status(501).send('conflict ocurred')
