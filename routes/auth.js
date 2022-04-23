a23c14fb26c9e23e69a2dbd575a89c48dd8614bd
alejo <alejorazeto@gmail.com>
2022-04-23 00:55:28 -0300

    jwt logic and register token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const AuthControllers = require('../controllers/authControllers')
 const { body, validationResult } = require('express-validator'); 
+const createJwt = require('../middlewares/jwtCreation')
 
 router.post(
     '/register', 
@@ -9,7 +10,8 @@ router.post(
     body('email', 'Please enter a valid mail').isEmail(), 
     body('image', 'Please enter the url of your photo').isURL(), 
     body('password', 'Please enter a valid password').isAlphanumeric(),
-    AuthControllers.signin
+    AuthControllers.signin,
+    createJwt
 );
 
 module.exports = router;
