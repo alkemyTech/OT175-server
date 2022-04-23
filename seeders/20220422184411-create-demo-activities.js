e6ffab939519747c11e871ad2f2f19f646ddf4c5
Rubén Marroquín <ruben9_1994@hotmail.com>
2022-04-22 23:30:46 -0500

    reverd added

--- seeders/20220422184411-create-demo-activities.js
+++ seeders/20220422184411-create-demo-activities.js
@@ -21,11 +21,11 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+    
+    
+    
+     await queryInterface.bulkDelete('People', null, {});
+    
   }
 };
