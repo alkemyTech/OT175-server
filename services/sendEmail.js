33dc657eabf2b9bcef28ae3452766884e5e32b44
Joaquin Miranda <joaquinmir99@gmail.com>
2022-04-23 18:06:54 -0300

    Added sendEmail service

---
+++ services/sendEmail.js
@@ -0,0 +1,24 @@
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+class SendEmail {
+  static async sendEmailTo(emailTo, emailSubject, emailTemplate) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: emailTo,
+      from: process.env.FROM_EMAIL,
+      subject: emailSubject,
+      html: emailTemplate,
+    };
+
+    try {
+      let res = await sgMail.send(msg);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  }
+}
+
+module.exports = SendEmail;
