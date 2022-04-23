f3dc9994fdd9fb97925760c32f36248070982613
alejo <alejorazeto@gmail.com>
2022-04-22 23:22:47 -0300

    slice --> split

--- middlewares/selfAuth.js
+++ middlewares/selfAuth.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 async function selfAuth(req, res, next){
     const {id} = req.params 
 
-    const token = req.headers.authorization.slice(7);
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const token = req.headers.authorization.split(" ");
+    const payload = jwt.verify(token[1], process.env.JWT_SECRET);
     if(payload.userId===parseInt(id)){       
         next()
     }else{
