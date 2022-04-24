9f19a72d3cdb4532ea68833c2cae98966a820192
alejo <alejorazeto@gmail.com>
2022-04-24 14:50:58 -0300

    revert unrelated changes

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const models = require('../models');
-const { User } = models
+const { Users } = models
 
 
 function restrictUnauthorizedRoles (authorizedRoles) {
     return async(req, res, next)=>{
-        const token = req.headers.authorization.slice(7);
+        const token = req.header;
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        let targetUser = {}
-        if(payload.userId && payload.roleId)
+        if(payload.id && payload.roleId)
         {
-            targetUser = await User.findOne({
+            const targetUser = await Users.findOne({
                 where:{
-                    id: payload.userId,
-                    roleId: payload.roleId
+                    id: payload.user,
+                    roleId: payload.role
                 }
             });
             if (targetUser && authorizedRoles.includes(targetUser.roleId)){
