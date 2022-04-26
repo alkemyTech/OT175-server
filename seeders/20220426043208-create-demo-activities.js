5f8595e60ee1827e269ffc0d485dea31915de995
Rubens94 <ruben9_1994@hotmail.com>
2022-04-25 23:33:02 -0500

    Seeder for activities

---
+++ seeders/20220426043208-create-demo-activities.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Activities', [{
+      name: 'Activitie test',
+      content: 'Demo for activities',
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+
+    await queryInterface.bulkInsert('Activities', [{
+      name: 'Activitie test 2',
+      content: 'Demo for activities 2',
+      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+    
+    
+    
+     await queryInterface.bulkDelete('Activities', null, {});
+    
+  }
+};
