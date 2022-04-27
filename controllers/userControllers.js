de2fd716805b10660b5a82d30aeb45b1f2d6d8f4
victor <vgrusz@yahoo.com.ar>
2022-04-27 12:42:52 +0200

    Ready for testing 2

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,8 @@
-const { NOT_FOUND } = require("../common/httpCodes");
-const HttpStatusCodes = require("../common/httpCodes");
-const Model = require("../models");
+const { NOT_FOUND } = require('../common/httpCodes');
+const HttpStatusCodes = require('../common/httpCodes');
+const Model = require('../models');
 const { User } = Model;
-const { body, param, validationResult } = require("express-validator");
+const { body, param, validationResult } = require('express-validator');
 
 class UserController {
   async getUsers(req, res, next) {
@@ -17,7 +17,9 @@ class UserController {
   async getUserById(req, res, next) {
     try {
       const userQuery = await User.findByPk(parseInt(req.params.id));
-      userQuery ? res.json(userQuery) : res.json({ msge: "The query got no results. Im sory" });
+      userQuery
+        ? res.json(userQuery)
+        : res.json({ msge: 'The query got no results. Im sory' });
     } catch (error) {
       next(error);
     }
@@ -25,8 +27,17 @@ class UserController {
 
   async updateUser(req, res, next) {
     const updates = Object.keys(req.body);
-    const allowedUpdates = ["firstName", "lastName", "email", "photo", "password", "roleId"];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    const allowedUpdates = [
+      'firstName',
+      'lastName',
+      'email',
+      'photo',
+      'password',
+      'roleId',
+    ];
+    const isValidOperation = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
 
     try {
       const adminQuery = await User.findAll({
@@ -35,7 +46,8 @@ class UserController {
         },
       });
 
-      if (!isValidOperation || !adminQuery) return res.status(400).send({ error: "Invalid update!" });
+      if (!isValidOperation || !adminQuery)
+        return res.status(400).send({ error: 'Invalid update!' });
       else if (isValidOperation) {
         const query = await User.update(req.body, {
           where: {
@@ -64,8 +76,12 @@ class UserController {
             id: req.params.id,
           },
         });
-        if (data) return res.json({ msge: "The user has been successfully deleted" });
-        else return res.status(404).json({ msge: "An error has occured. The user doesnt exist" });
+        if (data)
+          return res.json({ msge: 'The user has been successfully deleted' });
+        else
+          return res
+            .status(404)
+            .json({ msge: 'An error has occured. The user doesnt exist' });
       }
     } catch (error) {
       next(error);
@@ -76,7 +92,9 @@ class UserController {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res
+        .status(HttpStatusCodes.BAD_REQUEST)
+        .json({ errors: errors.array() });
     } else {
       let userId = req.params.id;
 
@@ -84,16 +102,22 @@ class UserController {
         let user = await User.findByPk(userId);
 
         if (!user) {
-          throw new Error("User not found");
+          throw new Error('User not found');
         }
+      } catch (error) {
+        return res
+          .status(HttpStatusCodes.NOT_FOUND)
+          .send({ status: error.message });
+      }
 
-        let firstName = req.body.firstName ? req.body.firstName : user.firstname;
-        let lastName = req.body.lastName ? req.body.lastName : user.lastName;
-        let email = req.body.email ? req.body.email : user.email;
-        let image = req.body.image ? req.body.image : user.image;
-        let password = req.body.password ? req.body.password : user.password;
-        let roleId = req.body.roleId ? req.body.roleId : user.roleId;
+      let firstName = req.body.firstName ? req.body.firstName : user.firstname;
+      let lastName = req.body.lastName ? req.body.lastName : user.lastName;
+      let email = req.body.email ? req.body.email : user.email;
+      let image = req.body.image ? req.body.image : user.image;
+      let password = req.body.password ? req.body.password : user.password;
+      let roleId = req.body.roleId ? req.body.roleId : user.roleId;
 
+      try {
         let result = await User.update(
           {
             firstName: firstName,
@@ -107,11 +131,13 @@ class UserController {
             where: { id: userId },
           }
         );
-
-        return res.send({ "Updated records: ": result });
       } catch (error) {
-        return res.status(HttpStatusCodes.NOT_FOUND).send({ status: error.message });
+        return res
+          .status(HttpStatusCodes.INTERNAL_SERVER_ERROR)
+          .send({ status: error.message });
       }
+
+      return res.send({ 'Updated records: ': result });
     }
   }
 }
