b132ec29a10f543b3358db9fe7f708c6097be5e8
victor <vgrusz@yahoo.com.ar>
2022-04-27 12:31:40 +0200

    Ready for testing

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,40 @@
-const router = require("express").Router();
-const UserController = require("../controllers/userControllers");
-const selfAuth = require("../middlewares/selfAuth");
+const router = require('express').Router();
+const UserController = require('../controllers/userControllers');
+const selfAuth = require('../middlewares/selfAuth');
 
-const { body, param, validationResult } = require("express-validator");
+const { body, param, validationResult } = require('express-validator');
 
-router.get("/", UserController.getUsers);
+router.get('/', UserController.getUsers);
 
-router.get("/:id", UserController.getUserById);
+router.get('/:id', UserController.getUserById);
 
-router.put("/update/:id", UserController.updateUser);
+router.put('/update/:id', UserController.updateUser);
 
-router.delete("/:id", selfAuth, UserController.deleteUser);
+router.delete('/:id', selfAuth, UserController.deleteUser);
 
 router.patch(
-  "/patch/:id",
+  '/patch/:id',
+  body('firstName').optional({ nullable: true }).isLength({ min: 1 }),
+  body('lastName').optional({ nullable: true }).isLength({ min: 1 }),
 
-  body("email", "debe ser un email válido").isEmail(),
-  body("password").isLength({ min: 1 }),
-  param("id", "id debe ser un número")
+  body('image').optional({ nullable: true }).isURL(),
+
+  body('email', 'Email must be a valid email')
+    .optional({ nullable: true })
+    .isEmail(),
+  body('password', 'Password must be minimun 5 character long')
+    .optional({ nullable: true })
+    .isLength({ min: 5 }),
+  param('id', 'Id must be a number')
+    .optional({ nullable: true })
     .exists()
     .toInt()
     .custom((id) => !isNaN(id)),
+  body('roleId', 'roleId must be a number')
+    .optional({ nullable: true })
+    .toInt()
+    .custom((id) => !isNaN(id)),
+
   UserController.patchUser
 );
 
