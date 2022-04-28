eae7049a2a33e9127944e59a8e043436d3fe8f1d
alejo <alejorazeto@gmail.com>
2022-04-28 12:07:08 -0300

    contact sender

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,21 +1,31 @@
 const models = require('../models');
 const { Contact } = models;
+const welcomeMail = require('../services/welcomeMail');
+
+const contact =
+  "Contactanos por los siguientes canales: Mail: somosfundacionmas@gmail.com, Instagram: SomosMás, Facebook: Somos_Más, Teléfono de contacto: 1160112988";
+
 
 class ContactsController {
     async create( req, res) {
         const { name, phone, email, message } = req.body;
-        await Contact.create({
-            name,
-            phone,
-            email,
-            message
-        });
-
-        try {
-            res.json({ 'msg': 'Contact created' });
+        try{
+            await Contact.create({
+                name,
+                phone,
+                email,
+                message
+            });
         } catch (err) {
             res.json({msg: 'Error to create contact'});
-        }
+        };
+        welcomeMail.sendWelcomeMail(
+            email, 
+            "Contact Added", 
+            "thank you for adding Contanct info",
+            'Your current contact info is ' + phone,
+            contact)
+        res.json({ 'msg': 'Contact created' });
     }
 }
 
