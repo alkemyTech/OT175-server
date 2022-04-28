d663a374d07eadbd9fc86be4a503a9e699c9fe1f
Rubens94 <ruben9_1994@hotmail.com>
2022-04-28 00:27:03 -0500

    create contact

---
+++ controllers/contacts.js
@@ -0,0 +1,22 @@
+const models = require('../models');
+const { Contact } = models;
+
+class ContactsController {
+    async create( req, res) {
+        const { name, phone, email, message } = req.body;
+        await Contact.create({
+            name,
+            phone,
+            email,
+            message
+        });
+
+        try {
+            res.json({ 'msg': 'Contact created' });
+        } catch (err) {
+            res.json({msg: 'Error to create contact'});
+        }
+    }
+}
+
+module.exports = new ContactsController();
