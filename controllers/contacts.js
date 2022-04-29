795ae766b7703c0c89c8275cc94c580ccd366898
alejo <alejorazeto@gmail.com>
2022-04-29 01:24:12 -0300

    files correction

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,11 @@
 const models = require('../models');
 const { Contact } = models;
 
+const welcomeMail = require('../services/welcomeMail');
+
+const contactText =
+  "Contactanos por los siguientes canales: Mail: somosfundacionmas@gmail.com, Instagram: SomosMás, Facebook: Somos_Más, Teléfono de contacto: 1160112988";
+
 class ContactsController {
     async create( req, res) {
         const { name, phone, email, message } = req.body;
@@ -11,11 +16,17 @@ class ContactsController {
             email,
             message
         });
-
-            res.json({ 'msg': 'Contact created' });
         } catch (err) {
             res.json({msg: 'Error to create contact'});
-        }
+        };
+        welcomeMail.sendWelcomeMail(
+            email, 
+            "Contact Added", 
+            "thank you for adding Contanct info",
+            'Your current contact info is ' + phone,
+            contactText
+            )
+        res.json({ 'msg': 'Contact created' });
     }
 
     async getContacts( req, res ) {
