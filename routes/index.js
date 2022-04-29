676d35351e389362f2046da24fee662bd32fe270
Rubens94 <ruben9_1994@hotmail.com>
2022-04-28 22:15:14 -0500

    route for backoffice

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersRoutes = require('./users')
 const authRoutes = require('./auth')
-const isAdminRole = require('../middlewares/adminAuthentication');
-const ContactsController = require('../controllers/contacts');
 
 
 /* GET home page. */
@@ -13,7 +11,5 @@ router.get('/', function(req, res, next) {
 
 router.use('/users', usersRoutes)
 router.use('/auth', authRoutes)
-
-router.get('/backoffice/contacts', [ isAdminRole ], ContactsController.getContacts );
 
 module.exports = router;
