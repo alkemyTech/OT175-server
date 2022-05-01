5547f1b06a4a1296f0fcb16f2e0af065d63f132d
victor <vgrusz@yahoo.com.ar>
2022-05-01 14:53:53 +0200

    backup 2

--- controllers/slidesController.js
+++ controllers/slidesController.js
@@ -1,5 +1,6 @@
 const Model = require('../models');
 const { Slide } = Model;
+const SlideController = require('../controllers/SlideController');
 
 class SlideController {
   async updateSlide(req, res, next) {}
--- routes/slides.js
+++ routes/slides.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const SlideController = require('../controllers/SlideController');
+const { body, param, validationResult } = require('express-validator');
 
-router.put('/update/:id', [isAdminRole], SlideController.updateSlide);
+router.put(
+  '/update/:id',
+  [isAdminRole],
+  param('id', 'Id must be a number')
+    .optional({ nullable: true })
+    .exists()
+    .toInt()
+    .custom((id) => !isNaN(id)),
+  SlideController.updateSlide
+);
 
 module.exports = router;
