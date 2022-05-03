5d6e5d4d7171374ae7d43741102f88e4d924ede6
victor <vgrusz@yahoo.com.ar>
2022-05-03 17:39:32 +0200

    Waiting for testimonials seeders and bug fix of clear password stored in DB to test

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,51 +1,84 @@
 const express = require('express');
-const Testimonials = require('../controllers/testimonials')
+const Testimonials = require('../controllers/testimonials');
 const router = express.Router();
 const isAdminRole = require('../middlewares/adminAuthentication');
+const { body, param, validationResult } = require('express-validator');
 
-router.get('/:id', function(req, res, next) {
-  Testimonials.get(req.params.id).then(result => {
+const { fieldsValidate } = require('../middlewares/fieldsValidate');
+
+router.get('/:id', function (req, res, next) {
+  Testimonials.get(req.params.id)
+    .then((result) => {
       res.json(result);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.get('/', function(req, res, next) {
-  Testimonials.index().then(result => {
-    res.json(result)
-  })
-  .catch(err => {
-    res.json(err);
-  })
+router.get('/', function (req, res, next) {
+  Testimonials.index()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.delete('/:id', [ isAdminRole ], function(req, res, next) {
-  Testimonials.delete(req.params.id).then(result => {
-    res.json(result);
-  })
-  .catch(err => {
-    res.json(err);
-  })
+router.delete('/:id', [isAdminRole], function (req, res, next) {
+  Testimonials.delete(req.params.id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.post('/',function(req,res,next) {
-  Testimonials.post(req.body.name,req.body.image,req.body.content).then(result => {
-      res.json(result);
-  })
-  .catch(err => {
-    res.json(err);
-  })
-})
+router.post(
+  '/',
+  [
+    isAdminRole,
+    body('name', 'name must exist and be minimum 1 character length').isLength({
+      min: 1,
+    }),
 
-router.patch('/:id',function(req,res,next) {
-  Testimonials.update(req.params.id,req.body.name,req.body.image,req.body.content).then(result => {
+    body('image', 'image must exist and be a URL').isURL(),
+
+    body(
+      'content',
+      'content must exist and be minimum 1 character length'
+    ).isLength({
+      min: 1,
+    }),
+
+    fieldsValidate,
+  ],
+  function (req, res, next) {
+    Testimonials.post(req.body.name, req.body.image, req.body.content)
+      .then((result) => {
+        res.json({ 'Records added ': result });
+      })
+      .catch((error) => {
+        res.json({ Error: error.message });
+      });
+  }
+);
+
+router.patch('/:id', function (req, res, next) {
+  Testimonials.update(
+    req.params.id,
+    req.body.name,
+    req.body.image,
+    req.body.content
+  )
+    .then((result) => {
       res.json(result);
-  })
-  .catch(err => {
-    res.json(err);
-  })
-})
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
