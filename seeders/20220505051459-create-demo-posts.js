a0183a335548b371bc1c99565e6bc3c8c3b09cf9
Rubens94 <ruben9_1994@hotmail.com>
2022-05-05 01:52:47 -0500

    CRUD post, model and seeder

---
+++ seeders/20220505051459-create-demo-posts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('posts', [{
+      title: 'Title post 1',
+      body: 'facebook.com/María_Irola',
+      image: 'https://res.cloudinary.com/dggzhar2j/image/upload/v1649968636/Mar%C3%ADa_Irola_eqgvwu.jpg',
+      userId: 1,
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+
+    await queryInterface.bulkInsert('posts', [{
+      title: 'Title post 2',
+      body: 'facebook.com/María_Irola',
+      image: 'https://res.cloudinary.com/dggzhar2j/image/upload/v1649968636/Mar%C3%ADa_Irola_eqgvwu.jpg',
+      userId: 1,
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+
+    await queryInterface.bulkInsert('posts', [{
+      title: 'Title post 3',
+      body: 'facebook.com/María_Irola',
+      image: 'https://res.cloudinary.com/dggzhar2j/image/upload/v1649968636/Mar%C3%ADa_Irola_eqgvwu.jpg',
+      userId: 1,
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('posts', null, {});
+  }
+};
