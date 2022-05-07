1b1d4c5f1c8e8ad73d2f4a4acb6c89d401fc1a5c
victor <vgrusz@yahoo.com.ar>
2022-05-07 16:46:52 +0200

    Ready to be tested

--- routes/category.js
+++ routes/category.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const CategoryController= require('../controllers/category')
-const restrictUnauthorizedRoles = require('../middlewares/userAuth')
-const validateId = require('../middlewares/validateId')
+const CategoryController = require('../controllers/category');
+const restrictUnauthorizedRoles = require('../middlewares/userAuth');
+const validateId = require('../middlewares/validateId');
 const isAdminRole = require('../middlewares/adminAuthentication');
- 
 
 /* GET categories listing. */
 
-router.get('/', CategoryController.list)
+router.get('/', CategoryController.list);
 // router.get('/:name', CategoryController.find)
-router.post('/', [ isAdminRole ], CategoryController.create)
-router.put('/update', CategoryController.update)
-router.delete('/:id', validateId, restrictUnauthorizedRoles([1]), CategoryController.remove)
+router.post('/', [isAdminRole], CategoryController.create);
+router.put('/update', [isAdminRole], CategoryController.update);
+router.delete(
+  '/:id',
+  [isAdminRole, validateId, restrictUnauthorizedRoles([1])],
+  CategoryController.remove
+);
 
 module.exports = router;
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -6,28 +6,36 @@ const isAdminRole = require('../middlewares/adminAuthentication');
 const { body, param, validationResult } = require('express-validator');
 const { fieldsValidate } = require('../middlewares/fieldsValidate');
 
-
-router.post('/',[isAdminRole,
-    
+router.post(
+  '/',
+  [
+    isAdminRole,
 
     body(
       'content',
       'content must exist and be minimum 1 character length'
     ).isLength({
-      min: 1,
+      min: 1
     }),
-        body("name", "Name can't be empty").notEmpty().trim(),
-        body("image", "Image must be a valid URL").isURL()
-    ],fieldsValidate, Testimonials.post);
+    body('name', "Name can't be empty").notEmpty().trim(),
+    body('image', 'Image must be a valid URL').isURL()
+  ],
+  fieldsValidate,
+  Testimonials.post
+);
 
 router.get('/', Testimonials.index);
 
-router.get('/:id',Testimonials.get);
+router.get('/:id', Testimonials.get);
 
-router.patch('/:id',[
-        body("image", "Image must be a valid URL").isURL()
-    ],fieldsValidate,Testimonials.update);
+router.patch(
+  '/:id',
+  [isAdminRole, body('image', 'Image must be a valid URL').isURL()],
+  fieldsValidate,
+  Testimonials.update
+);
 
-router.delete('/:id',Testimonials.delete);
+router.delete('/:id', [isAdminRole], Testimonials.delete);
 
 module.exports = router;
+const isAdminRole = require('../middlewares/adminAuthentication');
