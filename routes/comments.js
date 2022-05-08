b1dad25c128230fc9ab8a3dcf2e1803a23d15bec
Estefany Diaz <estefanydiaz75@gmail.com>
2022-05-07 22:30:18 -0500

    sólo el administrador puede ver todos los comentarios

--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const { fieldsValidate } = require('../middlewares/fieldsValidate');
+const isAdminRole = require('../middlewares/adminAuthentication');
 
 const CommentController = require('../controllers/comments');
 
@@ -10,5 +11,7 @@ router.put('/:id', [
     check('body', 'name must be string').isString().trim().escape(),
     fieldsValidate
 ], CommentController.updateCommentById);
+
+router.get('/', [isAdminRole], CommentController.getAllComments)
 
 module.exports = router;
