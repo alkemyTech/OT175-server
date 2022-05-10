195057035b4504eedcdd90cb57b314a9f8067de2
Joaquin Miranda <joaquinmir99@gmail.com>
2022-05-09 21:46:37 -0300

    Added members pagination

--- controllers/members.js
+++ controllers/members.js
@@ -3,20 +3,49 @@ const { Member } = models;
 const httpCodes = require('../common/httpCodes');
 const handleError = require('../common/handleError');
 class MemberController {
+
   static async getMembers(req, res) {
+    let page = Number.parseInt(req.query.page);
+    let members;
+    if(!page)
+      page = 1
     try {
-      const members = await Member.findAll({});
-
-      if (!members.length)
-        return res
-          .status(httpCodes.NOT_FOUND)
-          .json({ msg: 'There is no registered members' });
-
-      res.status(httpCodes.OK).json(members);
+      members = await Member.findAll({
+        limit: 10,
+        offset: 10*(page-1)
+      });
     } catch (err) {
-      console.log(err);
       return handleError.HTTP_ERROR_INTERNAL(err, res);
     }
+    if (!members.length)
+      return res
+        .status(httpCodes.NOT_FOUND)
+        .json({ msg: 'There is no members to show' });
+
+    const response = {
+      members: members
+    }
+
+    if (members.length === 10){
+      let nextMember; 
+      try {
+        nextMember = await Member.findAll({
+          limit: 1,
+          offset: 10*page
+        })
+      } catch (err) {
+        return handleError.HTTP_ERROR_INTERNAL(err, res);
+      }
+      if(nextMember.length > 0)
+        response.nextPage = `${process.env.BASE_PATH}/members?page=${page+1}`
+    }
+
+    if(page > 1){
+      response.previousPage = `${process.env.BASE_PATH}/members?page=${page-1}`
+    }
+
+    res.status(httpCodes.OK).json(response);
+    
   }
 
   static async getMemberById(req, res) {
