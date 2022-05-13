50086046898613f761b134a25cea9a8d8e347515
alejo <alejorazeto@gmail.com>
2022-05-12 21:20:42 -0300

    conection

--- test/users.test.js
+++ test/users.test.js
@@ -2,7 +2,21 @@ const chai= require('chai');
 const chaiHttp = require('chai-http');
 
 const expect = chai.expect;
-const should = chai.should;
-const server = require('../app');
+const should = chai.should();
+
+const app = require('../app');
+
 
 chai.use(chaiHttp)
+
+describe('Testing users ...', ()=>{
+    it('users/get ... ', (done)=>{
+        chai.request(app)
+        .get('/users')
+        .end((err, res)=>{
+            res.should.have.status(200);
+
+            done();
+        })
+    });
+})
