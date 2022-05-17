687e986f7f0156698e99bfb7ac2169267cd9e671
victor <vgrusz@yahoo.com.ar>
2022-05-17 10:55:50 +0200

    OT175-98 backup 05

--- test/route.news.test.js
+++ test/route.news.test.js
@@ -10,7 +10,7 @@ const app = require('../app');
 const NewsController = require('../controllers/news.controllers');
 
 describe('News route test', function () {
-  describe('Should call news controller, method createNews', function () {
+  describe('Should call news controller, method getAll', function () {
     it('should call stubed function at least once', async function () {
       const newsController = new NewsController();
 
