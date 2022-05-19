a4262d5c2704b125390173bd9d8c82148b9c88c5
alejo <alejorazeto@gmail.com>
2022-05-18 22:13:24 -0300

    testAuth + auth endpoint and controllers cleaner

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -13,14 +13,9 @@ const contact =
   "Contactanos por los siguientes canales: Mail: somosfundacionmas@gmail.com, Instagram: SomosMás, Facebook: Somos_Más, Teléfono de contacto: 1160112988";
 
 class AuthControllers {
-  async signin(req, res, next) {
+  static async signin(req, res, next) {
     const title = `Bienvenido ${req.body.firstName}`
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
-
       const newUser = await User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -29,8 +24,16 @@ class AuthControllers {
         password: req.body.password,
         roleId: 2,
       });
-      res.locals.userData = newUser;     
-      next();
+      let {id, roleId} = newUser;
+      const token = jwt.sign({ id, roleId }, process.env.JWT_SECRET, {
+        expiresIn: "8h",
+      });
+      res
+        .status(200)
+        .json({
+          ...newUser.dataValues,
+          token :token
+        });
     } catch (error) {
       next(error);
     }
@@ -48,10 +51,9 @@ class AuthControllers {
     
   }
 
-  async getDataUser(req, res) {
+  static async getDataUser(req, res) {
     const token = req.headers.authorization.split(" ")[1];
     const jwtDecoded = jwt.verify(token, process.env.JWT_SECRET);
-
     const user = await User.findOne({
       where: { id: jwtDecoded.id },
       include: [
@@ -63,21 +65,16 @@ class AuthControllers {
     });
 
     try {
-      res.json(user);
+      res.status(200).json(user);
     } catch (err) {
-      console.log(err);
       res.json({
         msg: "There was a problem getting the user data, check with the administrator",
       });
     }
   }
 
-  async login(req, res, next) {
+  static async login(req, res, next) {
     try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        throw new Error("Invalid email or password");
-      }
       const { email } = req.body;
       let user = await User.findOne({ where: { email } });
 
@@ -85,23 +82,23 @@ class AuthControllers {
         throw new Error("User/email not found");
       } else {
         let passwordIsOk = bcrypt.compareSync(req.body.password, user.password);
-
+        console.log(passwordIsOk)
         if (passwordIsOk) {
           const { id, roleId } = user;
           const token = jwt.sign({ id, roleId }, process.env.JWT_SECRET, {
             expiresIn: "8h",
           });
 
-          res.json({ token });
+          res.status(200).json( token );
         } else {
           throw new Error("Invalid password");
         }
       }
     } catch (error) {
-      res.status(httpStatusCodes.NOT_FOUND).send({ ok: false });
+      res.status(httpStatusCodes.NOT_FOUND).json({ ok: false });
       console.log(error);
     }
   }
 }
 
-module.exports = new AuthControllers();
+module.exports = AuthControllers;
---
+++ test/authentication.test.js
@@ -0,0 +1,139 @@
+const { expect } = require('chai');
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
+
+const models = require('../models');
+const {  User, Role } = models;
+
+const AuthControllers = require('../controllers/authControllers');
+const welcomeMail = require("../services/welcomeMail");
+
+describe('Testing users  ...', ()=>{
+    const testUserList=[
+        {
+            "id": 1,
+            "firstName": "Usuario01",
+            "lastName": "Demo02",
+            "email": "test01@test.com",
+            "image": "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+            "password": "$2b$08$/FPf4u.c.flcDSqFB2xvBuvS0s6xTsVW0d1CBGj5PbRc3hM2O8rjS",
+            "roleId": 1,
+            "deletedAt": null,
+            "createdAt": "2022-05-11T15:42:08.000Z",
+            "updatedAt": "2022-05-11T15:42:08.000Z",
+            "realPassword": "testpassword"
+        },
+        {
+            "id": 2,
+            "firstName": "Usuario02",
+            "lastName": "Demo03",
+            "email": "test03@test.com",
+            "image": "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+            "password": "$2b$08$/FPf4u.c.flcDSqFB2xvBuvS0s6xTsVW0d1CBGj5PbRc3hM2O8rjS",
+            "roleId": 1,
+            "deletedAt": null,
+            "createdAt": "2022-05-11T15:42:08.000Z",
+            "updatedAt": "2022-05-11T15:42:08.000Z",
+            "realPassword": "testpassword"
+        },
+        {
+            "id": 3,
+            "firstName": "Usuario03",
+            "lastName": "Demo04",
+            "email": "test04@test.com",
+            "image": "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
+            "password": "$2b$08$/FPf4u.c.flcDSqFB2xvBuvS0s6xTsVW0d1CBGj5PbRc3hM2O8rjS",
+            "roleId": 1,
+            "deletedAt": null,
+            "createdAt": "2022-05-11T15:42:08.000Z",
+            "updatedAt": "2022-05-11T15:42:08.000Z",
+            "realPassword": "testpassword"
+        }
+    ];
+    const rndId = Math.floor(Math.random() * testUserList.length);
+    const invalidId = testUserList.length + 1;
+    let req = {};
+    let status, json, res ;
+    beforeEach(() => {
+        status = sinon.stub();
+        json = sinon.spy();
+        res = { json, status };
+        status.returns(res);
+    });
+    
+    afterEach(()=> { 
+        sinon.verifyAndRestore(); 
+    });
+    describe('Testing auth (success cases)...', ()=>{
+        it('signin',async()=>{
+            req = {
+                body:{ 
+                    "firstName": "New User",
+                    "lastName": "Not real",
+                    "email": "testing@test.com",
+                    "image": "image.png",
+                    "password": "12345"
+                }
+            };
+            let newUser ={
+                dataValues:{
+                    ...req.body,
+                    id: testUserList.length + 1,
+                    roleId: 2
+                }
+            };
+            let stubCreate = sinon.stub(User, 'create')
+                .returns(newUser);
+            let stubMail = sinon.stub(welcomeMail, 'sendWelcomeMail')
+                .returns(true);
+            let stubToken = sinon.stub(jwt, 'sign')
+                .returns('token');
+            await AuthControllers.signin(req, res)
+            expect(stubCreate.calledOnce).to.be.true;
+            expect(stubMail.calledOnce).to.be.true;
+            expect(stubToken.calledOnce).to.be.true;
+            expect(json.calledWith({
+                ...newUser.dataValues,
+                token: 'token'
+                }
+            )).to.be.true;
+            expect(status.calledWith(200)).to.be.true;
+        });
+        it('getDataUser', async()=>{
+            req = {
+                headers: {authorization: 'Bearer token'},
+            };
+            let decodedPayload ={
+                    id: rndId,
+                    roleId: 2 
+            };
+            let stubToken = sinon.stub(jwt, 'verify')
+                .returns(decodedPayload);
+            let stub = sinon.stub(User, 'findOne')
+                .returns(testUserList[decodedPayload.id]);
+            await AuthControllers.getDataUser(req, res)
+            expect(stubToken.calledOnce).to.be.true;
+            expect(stub.calledOnce).to.be.true;
+            expect(json.calledWith(testUserList[decodedPayload.id]));
+            expect(status.calledWith(200)).to.be.true;
+        });
+        it(`login/${rndId}`, async()=>{
+            let targetUser = testUserList[rndId]
+            req = {
+                body:{ 
+                    "email": targetUser.email,
+                    "password": targetUser.realPassword
+                }
+            };
+            let stub = sinon.stub(User, 'findOne')
+                .returns(targetUser);
+            let stubToken = sinon.stub(jwt, 'sign')
+                .returns('token');
+            await AuthControllers.login(req, res)
+            expect(stub.calledOnce).to.be.true;
+            expect(stubToken.calledOnce).to.be.true;
+            expect(json.calledWith('token')).to.be.true;
+            expect(status.calledWith(200)).to.be.true
+        })
+    });
+});
